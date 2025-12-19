4c464bb735615dfa6f3ab260059fde7f6a1178a0
simsieec025 <simsieec025@student.wethinkcode.co.za>
2025-12-19 23:50:14 +0200

    indeed to use int for opening and closing time

--- access_check.py
+++ access_check.py
@@ -3,16 +3,16 @@ import datetime
 role = input("Enter your role: ").strip().lower()
 has_token = input("Do you have a token, yes or no? ").strip().lower()
 current_hour = datetime.datetime.now().hour
-opening_time = datetime.time(9 , 0)
-closing_time = datetime.time(17 , 0)
+opening_time = 9
+closing_time = 17
 
 def access_check(role, has_token):
     if role == "admin" and has_token == "yes":
         print("ACCESS GRANTED")
     elif role == "editor" and has_token == "yes":
-        if opening_time <= current_hour <= closing_time:
+        if opening_time <= current_hour < closing_time:
             print("ACCESS GRANTED")
-        elif not (opening_time <= current_hour <= closing_time):
+        elif not (opening_time <= current_hour < closing_time):
             print("ACCSS DENIED out of office hours")
         else:
             print("ACCESS DENIED missing credentails")
